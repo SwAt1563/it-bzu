4fc088d5bbdd4c52160966fdcef2a2b31a3b2435
Qutaiba Olayyan <swat.ar123@gmail.com>
2022-12-15 21:10:08 +0200

    updated

--- src/Components/Offer/Offer.js
+++ src/Components/Offer/Offer.js
@@ -1,7 +1,7 @@
 import "./Offer.css";
 import Fade from "react-reveal/Fade";
 import { Link } from "react-router-dom";
-import { FaFacebook } from "react-icons/fa";
+import { FaFacebook, FaCaretDown, FaCaretUp } from "react-icons/fa";
 
 const Offer = (props) => {
   const side =
@@ -11,6 +11,56 @@ const Offer = (props) => {
   return (
     <>
       <Fade bottom>
+        <div className="offer mb-3">
+          <div className="container">
+            <details>
+              <summary>
+                <div className="smry row">
+                  <div className="title col-10">
+                    <h2 className="name mb-2">
+                      {props.name}
+                      <a className="site fs-3 me-3" href={props.site}>
+                        <FaFacebook />
+                      </a>
+                    </h2>
+                    <div className="date mb-3">
+                      العرض ساري من {props.date_from} الى {props.date_to}
+                    </div>
+                  </div>
+
+                  <div className="scroll-icon fs-1  col-2 d-flex align-items-center justify-content-center">
+                    <div className="s-down"><FaCaretDown /></div>
+                    <div className="s-up"><FaCaretUp/></div>
+                  </div>
+                </div>
+              </summary>
+
+              <div className={side}>
+                <div className="photo d-flex align-items-center justify-content-center col-lg-4 col-sm-12  mb-3">
+                  <img
+                    className="img-fluid "
+                    src={props.logo}
+                    alt={props.name}
+                  />
+                </div>
+                <div className="content col-lg-8 col-sm-12 ">
+                  <div className="time">اوقات الدوام: {props.time}</div>
+                  <div className="number">رقم المحل: {props.number}</div>
+                  <div className="position mb-5">العنوان: {props.position}</div>
+
+                  <p className="about">{props.descripition}</p>
+                  <ul className="offers ">
+                    {props.offers.map((offer) => (
+                      <li>{offer}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </details>
+          </div>
+        </div>
+      </Fade>
+      {/* <Fade bottom>
         <div className="offer mb-5">
           <div className="container">
             <div className={side}>
@@ -42,7 +92,7 @@ const Offer = (props) => {
             </div>
           </div>
         </div>
-      </Fade>
+      </Fade> */}
     </>
   );
 };
