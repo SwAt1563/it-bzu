5485ec57f74024d6cc290a7dc6c4dbe216080424
Qutaiba Olayyan <swat.ar123@gmail.com>
2023-01-19 19:06:04 +0200

    video

--- src/Components/Offer_Video/Offer_Video.js
+++ src/Components/Offer_Video/Offer_Video.js
@@ -3,13 +3,17 @@ import "./Offer_Video.css";
 const Offer_Video = (props) => {
   return (
     <>
-      <div className="video mt-3 p-5">
-        <div className=" row ">
-          <div className="part mb-3 col-lg-6 col-sm-12 d-flex align-items-center justify-content-center">
-            <video className="" id="video_id" poster={props.poster}>
-              <source src={props.video} type="video/mp4" />
-            </video>
-
+      <div className="offer_video mt-3 p-5">
+        <div className=" row d-flex align-items-center justify-content-center">
+          <div className="mb-3 col-lg-6 col-sm-12 ">
+            <video
+              src={props.video}
+              type="video/mp4"
+              className="img-fluid"
+              id="video_id"
+              poster={props.poster}
+              controls
+            ></video>
           </div>
         </div>
       </div>
